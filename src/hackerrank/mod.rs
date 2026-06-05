63ce2c9c49ab757ef957e637d2801fac3529148a
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2026-06-05 14:16:16 +0300

    task 2

--- src/hackerrank/mod.rs
+++ src/hackerrank/mod.rs
@@ -1,5 +1,5 @@
 mod task0;
-mod task03;
+mod task02;
 mod task04;
 mod task05;
 mod task06;
--- src/hackerrank/task02.rs
+++ src/hackerrank/task02.rs
@@ -1,1 +1,4 @@
 // https://www.hackerrank.com/challenges/staircase/problem
+fn staircase(n: i32) {
+    return;
+}
